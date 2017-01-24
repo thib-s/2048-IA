6dc07fceee54618705ad955b62904e6788fc8764
Thibaut Boissin <thibaut.boissin@gmail.com>
2017-01-24 09:09:51 +0100

    untrained basic NN

--- strategy.py
+++ strategy.py
@@ -2,19 +2,23 @@
 implementation des strategies de jeu pour placer les tuiles et jouer
 """
 # imports ###################################################################
+import numpy as np
 
-from multiprocessing import Pool
 import logic
 import random
-import score_utility
+from pybrain.structure import RecurrentNetwork, LinearLayer, SigmoidLayer, FullConnection
 
 # configuration ############################################################
 
 NEW_TILE_STRATEGY = 'random'
 
-DIRECTION_STRATEGY = 'keyboard'
+DIRECTION_STRATEGY = 'brain'
 
 SCORE_FUNCTION = 'ecart'
+
+BRAIN_INIT = False
+
+NET = RecurrentNetwork()
 
 #--------------------------------------#
 #---------CHOIX DES STRATEGIES---------#
@@ -35,6 +39,8 @@ def choose_direction(key, board):
     """selectionne la strategie a utiliser pour choisir la direction"""
     if DIRECTION_STRATEGY == 'keyboard':
         return keyboard_choose_direction(key)
+    elif DIRECTION_STRATEGY == "brain":
+        return brain_choose_direction(board)
     else:
         raise ValueError('No such strategy', DIRECTION_STRATEGY)
 
@@ -59,6 +65,48 @@ def keyboard_choose_direction(key):
     except KeyError:
         return None
     return direction
+
+def brain_choose_direction(board):
+    global BRAIN_INIT
+    if not BRAIN_INIT:
+        brain_init()
+        BRAIN_INIT = True
+    return brain_decision(board)
+
+def brain_init():
+    global NET
+    NET.addInputModule(LinearLayer(logic.SIZE**2, name='in'))
+    NET.addModule(SigmoidLayer(logic.SIZE**2, name='hidden'))
+    NET.addOutputModule(LinearLayer(4, name='out'))
+    NET.addConnection(FullConnection(NET['in'], NET['hidden'], name='c1'))
+    NET.addConnection(FullConnection(NET['hidden'], NET['out'], name='c2'))
+    NET.addRecurrentConnection(FullConnection(NET['hidden'], NET['hidden'], name='c3'))
+    NET.sortModules()
+    NET.randomize()
+    #set_brain_weights()
+
+def set_brain_weights(weights):
+    global NET
+    NET.params = weights
+
+def get_brain_weights():
+    return NET.params
+
+def brain_decision(board):
+    global NET
+    cp_board = prepare_board(board)
+    result = NET.activate(cp_board)
+    max_val = -float('inf')
+    max_index = 0
+    for i in range(len(result)):
+        if result[i]>=max_val and logic.move_is_possible(logic.DIRECTIONS[i],board):
+            max_index = i
+            max_val = result[i]
+    return logic.DIRECTIONS[max_index]
+
+def prepare_board(board):
+    return np.asarray(board).reshape(-1)
+
 
 #--------------------------------------#
 #-----------CHOIX DES TUILES-----------#
